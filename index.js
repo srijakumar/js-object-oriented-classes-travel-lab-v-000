646f092fcc305700efe85e689694a2683b5612b4
Srija S <srijakumar7@gmail.com>
2018-02-19 00:18:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ class Driver {
     this.startDate = new Date(startDate);
 }
 }
-/*  yearsExperienceFromBeginningOf(year){
-    let endDate = new Date(year, 1, 1);
-    let totalYears = (endDate - this.startDate);
-    return parseInt(totalYears);
-  }
 
-/*
-let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-
-class Route(){
-  constructor(beginningLocation, endingLocation){
-    this.beginningLocation = beginningLocation
-    this.endingLocation = endingLocation
-  }
-
-blocksTravelled(avenue, street){
-  let distance1= avenue - indexOf(beginningLocation)
-
-}
-
-}
+// yearsExperienceFromBeginningOf(year){
+//     let endDate = new Date(year, 1, 1);
+//     let totalYears = (endDate - this.startDate);
+//     return parseInt(totalYears);
+//   }
+// 
+// let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+// 
+// class Route(){
+//   constructor(beginningLocation, endingLocation){
+//     this.beginningLocation = beginningLocation
+//     this.endingLocation = endingLocation
+//   }
+// 
+// blocksTravelled(avenue, street){
+//   let distance1= avenue - indexOf(beginningLocation)
+// 
+// }
+// 
+// }
