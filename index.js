b6989a2b05860d934cd0270c258367fb0fcd4cbd
Srija S <srijakumar7@gmail.com>
2018-02-19 00:31:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ yearsExperienceFromBeginningOf(year){
     return Math.ceil(totalYears);
   }
 }
-
-let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-
-class Route(){
-  constructor(beginningLocation, endingLocation){
-    this.beginningLocation = beginningLocation
-    this.endingLocation = endingLocation
-  }
-
-blocksTravelled(avenue, street){
-  let distance1= avenue - indexOf(beginningLocation)
-
-}
-
-}
+//
+// let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+//
+// class Route(){
+//   constructor(beginningLocation, endingLocation){
+//     this.beginningLocation = beginningLocation
+//     this.endingLocation = endingLocation
+//   }
+//
+// blocksTravelled(avenue, street){
+//   let distance1= avenue - indexOf(beginningLocation)
+//
+// }
+//
+// }
