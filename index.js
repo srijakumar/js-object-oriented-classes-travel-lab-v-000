702731c79a12b76f7a382d322490bcc28b9baec1
Srija S <srijakumar7@gmail.com>
2018-02-19 00:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ class Driver {
   constructor (name, startDate){
     this.name = name;
     this.startDate = new Date(startDate);
-
-
+}
   yearsExperienceFromBeginningOf(year){
     let endDate = new Date(year, 1, 1);
     let totalYears = (endDate - this.startDate);
